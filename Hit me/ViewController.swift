8560f27e5f9c33ad630c0712ce5b794e31bd217a
Andrey1224 <nepod77@gmail.com>
2021-07-07 15:54:09 -0400

    working 1

--- Hit me/ViewController.swift
+++ Hit me/ViewController.swift
@@ -9,39 +9,82 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var roundLabel: UILabel!
     
-    var currentValue: Int = 0
-
+    
+    var round = 0
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBOutlet weak var targetLabel: UILabel!
+    
+    @IBOutlet var slider: UISlider!
+    
+    var targetValue = 0
+    
+    var currentValue = 0
+    
+    var score = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        startNewRound()
     }
     
     
     @IBAction func showAlert() {
         
+        
+        
+        let difference = abs(targetValue - currentValue)
+        let points = 100 - difference
+        
+        score += points
+        
+        
+        
+        
+        let message = "You scored \(points) points"
+        
         let alert = UIAlertController(
             title: "Hello, World",
-            message: "This is my first app!",
+            message: message,
             preferredStyle: .alert)
         
         let action = UIAlertAction(
-            title: "Awesome",
+            title: "OK",
             style: .default,
             handler: nil)
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        
+        
+        startNewRound()
     }
-     
+    
     
     @IBAction func sliderMoved(_ slider: UISlider) {
-        currentValue = lround(slider.value)
+        currentValue = lround(Double(slider.value))
+        
     }
-
     
     
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+        round += 1
+      
+    }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+    }
     
     
     
